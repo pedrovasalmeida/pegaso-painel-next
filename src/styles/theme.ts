151c478ab1188ac09b9d04aa50c97afc34151766
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-04-20 20:08:00 -0300

    fix: :lipstick: fixing global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,30 +1,30 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, useColorModeValue } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   colors: {
-    blue: {
-      '900': '#042330',
-      '800': '#063447',
-      '700': '#09455e',
-      '600': '#0e6387',
-      '500': '#0f6e96',
-      '400': '#4b839c',
-      '300': '#78a1b3',
-      '200': '#97b7c4',
-      '100': '#b8cfd9',
-      '50': '#e6eff2',
-    },
     gray: {
-      '900': '#181B23',
-      '800': '#1F2029',
-      '700': '#353646',
-      '600': '#4B4D63',
-      '500': '#616480',
-      '400': '#797D9A',
+      '900': '#182024',
+      '800': '#1f2629',
+      '700': '#364247',
+      '600': '#4b5c63',
+      '500': '#617680',
+      '400': '#79919c',
       '300': '#9699B0',
       '200': '#B3B5C6',
       '100': '#D1D2DC',
       '50': '#EEEEF2',
+    },
+    blue: {
+      '900': '#1a485c',
+      '800': '#295263',
+      '700': '#0e6387',
+      '600': '#2a88b0',
+      '500': '#32a0cf',
+      '400': '#41b1e0',
+      '300': '#61c3ed',
+      '200': '#8fd5f2',
+      '100': '#bce6f7',
+      '50': '#e8f6fc',
     },
   },
   fonts: {
@@ -32,11 +32,15 @@ export const theme = extendTheme({
     body: 'Roboto',
   },
   styles: {
-    global: {
+    global: (props) => ({
       body: {
-        bg: 'gray.900',
-        color: 'blue.50',
+        bg: props.colorMode === 'light' ? 'gray.50' : 'gray.900',
+        color: props.colorMode === 'light' ? 'gray.900' : 'gray.50',
       },
-    },
+    }),
+  },
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
   },
 });
