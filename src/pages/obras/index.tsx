ff6ea999d2ac165e2db25d83ce7cbd09da381d82
pedrovasalmeida <pedrovasalmeida@gmail.com>
2021-05-02 19:53:54 -0300

    feat: :bug: rendering a temporary message when access route /obras

--- src/pages/obras/index.tsx
+++ src/pages/obras/index.tsx
@@ -54,6 +54,23 @@ export default function UserList() {
   }
 
   return (
+    <Flex direction="column" h="100vh" w="100vw" bg="gray.50">
+      <Text
+        mx="auto"
+        mt="auto"
+        fontSize="30"
+        fontWeight="bold"
+        color="gray.700"
+      >
+        Página em construção!
+      </Text>
+      <Text mx="auto" mt="2" mb="auto" fontSize="15" color="gray.500">
+        Pégaso - 2021 &copy;
+      </Text>
+    </Flex>
+  );
+
+  return (
     <Box>
       <Header />
 
