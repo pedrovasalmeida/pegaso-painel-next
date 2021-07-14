e83474e9bde23ad87369b54e01be9f1c6c6d7659
Pedro Henrique <pedro.henrique@clippingcacd.com.br>
2021-07-14 17:55:01 -0300

    WIP

--- src/pages/api/editEnterprise.ts
+++ src/pages/api/editEnterprise.ts
@@ -1,40 +1,41 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { query as q } from 'faunadb';
+// import { query as q } from 'faunadb';
 import { fauna } from '../../services/fauna';
 
 import { IFinalEnterprise } from '../../types/IEnterprise';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const enterpriseRef = req.body.ref;
-    const data: IFinalEnterprise = req.body;
+  console.log('edit api');
+  // try {
+  //   const enterpriseRef = req.body.ref;
+  //   const data: IFinalEnterprise = req.body;
 
-    const enterpriseData = {
-      id: data.id,
-      name: data.name,
-      description: data.description,
-      shortDescription: data.shortDescription,
-      address: data.address,
-      banner: data.banner,
-      displayOrder: data.displayOrder,
-      images: data.images,
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt,
-    };
+  //   const enterpriseData = {
+  //     id: data.id,
+  //     name: data.name,
+  //     description: data.description,
+  //     shortDescription: data.shortDescription,
+  //     address: data.address,
+  //     banner: data.banner,
+  //     displayOrder: data.displayOrder,
+  //     images: data.images,
+  //     createdAt: data.createdAt,
+  //     updatedAt: data.updatedAt,
+  //   };
 
-    await fauna.query(
-      q.Update(q.Ref(q.Collection('enterprises'), enterpriseRef), {
-        data: {
-          ...enterpriseData,
-        },
-      })
-    );
+  //   await fauna.query(
+  //     q.Update(q.Ref(q.Collection('enterprises'), enterpriseRef), {
+  //       data: {
+  //         ...enterpriseData,
+  //       },
+  //     })
+  //   );
 
-    return res.status(200).json({ message: 'Dados atualizados com sucesso.' });
-  } catch {
-    return res.status(400).json({
-      error: 'Houve um problema ao atualizar os dados.',
-      errorCode: 'cannot.update.enterprise',
-    });
-  }
+  //   return res.status(200).json({ message: 'Dados atualizados com sucesso.' });
+  // } catch {
+  //   return res.status(400).json({
+  //     error: 'Houve um problema ao atualizar os dados.',
+  //     errorCode: 'cannot.update.enterprise',
+  //   });
+  // }
 };
